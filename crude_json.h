8cd34aa67ad86ddcff0fea5b1b433901b7a37b7c
Jefferson Romney <jefferson@flightlevelengineering.com>
2026-08-15 02:26:13 -0600

    Fix warnings

--- crude_json.h
+++ crude_json.h
@@ -1,4 +1,4 @@
-﻿// Crude implementation of JSON value object and parser.
+// Crude implementation of JSON value object and parser.
 //
 // VERSION 0.1
 //
@@ -147,7 +147,9 @@ private:
 # undef CRUDE_MAX4
 # undef CRUDE_MAX3
 # undef CRUDE_MAX2
-    using storage_t = std::aligned_storage<max_size, max_align>::type;
+    struct alignas(max_align) storage_t {
+        unsigned char data[max_size];
+    };
 
     static       object*   object_ptr(      storage_t& storage) { return reinterpret_cast<       object*>(&storage); }
     static const object*   object_ptr(const storage_t& storage) { return reinterpret_cast<const  object*>(&storage); }
